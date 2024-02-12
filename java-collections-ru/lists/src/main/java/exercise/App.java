af86c6704f057ab1f8f8ac21b7f5525d17a72956
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-02-12 16:39:03 +0300

    дз1

---
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -0,0 +1,27 @@
+package exercise;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+// BEGIN
+public class App {
+    public static boolean scrabble(String chars, String word) {
+        var newChars = chars.toLowerCase().toCharArray();
+        var charList = new ArrayList<Character>();
+        for (var c : newChars) {
+            charList.add(c);
+        }
+        var newWord = word.toLowerCase().toCharArray();
+        var charOfWord = new ArrayList<Character>();
+        for (var w : newWord) {
+            charOfWord.add(w);
+        }
+        if (charList.containsAll(charOfWord)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+//END
