4a1739633e725e028d8cb68b2e1d0eb1354e7742
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-02-15 16:42:50 +0300

    дз3

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -3,11 +3,12 @@ package exercise;
 import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
-import java.util.Map.Entry;
+//import java.util.Map.Entry;
 
 // BEGIN
 public class App {
-    public static Map<String, String> findWhere(List<Map<String, String>> listOfBooks, Map<String, String> filterValues) {
+    public static Map<String, String> findWhere(List<Map<String, String>> listOfBooks,
+                                                Map<String, String> filterValues) {
         List<String> result = new ArrayList<>();
         var i = 0;
         for (var list : listOfBooks) {
