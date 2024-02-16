915e1cdddc1380af894cbcdfb32a31add1e8fdba
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-02-16 17:43:53 +0300

    дз4

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -1,10 +1,10 @@
 package exercise;
 
-//import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
-//import java.util.Arrays;
+import java.util.Arrays;
 import java.util.ArrayList;
-//import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ArrayBlockingQueue;
 
 import org.junit.jupiter.api.Test;
 
