dfd7bccf010155917df97fcc9a919f2d2dee5936
@hexlet/cli <support@hexlet.io>
2024-02-29 14:11:25 +0300

    submit java-collections-ru/other-collections

---
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -0,0 +1,33 @@
+package exercise;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+// BEGIN
+public class App {
+    public static LinkedHashMap<String, String> genDiff(Map<String, Objects> dictionaryOne,
+                                                        Map<String, Objects> dictionaryTwo) {
+        Map<String, String> result;
+        result = dictionaryOne.entrySet().stream()
+                .filter(e -> !dictionaryTwo.containsKey(e.getKey()))
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> "deleted"));
+
+        result.putAll(dictionaryTwo.entrySet().stream()
+                .filter(e -> !dictionaryOne.containsKey(e.getKey()))
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> "added")));
+
+        result.putAll(dictionaryOne.entrySet().stream()
+                .filter(e -> dictionaryTwo.containsKey(e.getKey()))
+                .filter(e -> dictionaryTwo.get(e.getKey()).equals(e.getValue()))
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> "unchanged")));
+
+        result.putAll(dictionaryOne.entrySet().stream()
+                .filter(e -> dictionaryTwo.containsKey(e.getKey()))
+                .filter(e -> !dictionaryTwo.get(e.getKey()).equals(e.getValue()))
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> "changed")));
+        LinkedHashMap<String, String> linkedHashMapResult = new LinkedHashMap<>(result);
+
+        return linkedHashMapResult;
+    }
+}
+//END
