8448d992877071417879dd4c617165b4079baff1
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-02-29 15:25:30 +0300

    дз8

--- java-collections-ru/other-collections/src/main/java/exercise/App.java
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -8,12 +8,11 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.Objects;
 
 // BEGIN
 public class App {
-    public static Map<String, String> genDiff(Map<String, Objects> dictionaryOne,
-                                                        Map<String, Objects> dictionaryTwo) {
+    public static Map<String, String> genDiff(Map<String, Object> dictionaryOne,
+                                                        Map<String, Object> dictionaryTwo) {
         Map<String, String> result = new LinkedHashMap<>();
         Set<String> keys = new TreeSet<>(dictionaryOne.keySet());
         keys.addAll(dictionaryTwo.keySet());
