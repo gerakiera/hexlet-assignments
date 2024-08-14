4d21e413e9670693ab3bd63038cb9ebc6b96245f
@hexlet/cli <support@hexlet.io>
2024-08-14 17:54:40 +0300

    submit java-spring-ru/integration-tests

--- java-spring-ru/integration-tests/src/test/java/exercise/controller/TaskControllerTest.java
+++ java-spring-ru/integration-tests/src/test/java/exercise/controller/TaskControllerTest.java
@@ -1,27 +1,27 @@
 package exercise.controller;
 
-import org.junit.jupiter.api.Test;
-import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
-import static org.assertj.core.api.Assertions.assertThat;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import exercise.model.Task;
+import exercise.repository.TaskRepository;
+import net.datafaker.Faker;
 import org.instancio.Instancio;
 import org.instancio.Select;
-
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.HashMap;
+
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-
-import java.util.HashMap;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import net.datafaker.Faker;
-import exercise.repository.TaskRepository;
-import exercise.model.Task;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 // BEGIN
 @SpringBootTest
@@ -83,7 +83,13 @@ class ApplicationTest {
                 json -> json.node("description").isEqualTo(task.getDescription())
         );
     }
-
+    private Task generateTask() {
+        return Instancio.of(Task.class)
+                .ignore(Select.field(Task::getId))
+                .supply(Select.field(Task::getTitle), () -> faker.lorem().word())
+                .supply(Select.field(Task::getDescription), () -> faker.lorem().paragraph())
+                .create();
+    }
     @Test
     public void testCreate() throws Exception {
         var data = generateTask();
