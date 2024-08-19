338e567e6f93069fa7534a8bf750aad6179252d6
@hexlet/cli <support@hexlet.io>
2024-08-19 16:42:16 +0300

    submit java-spring-ru/authentication

--- java-spring-ru/authentication/src/main/java/exercise/controller/ArticleController.java
+++ java-spring-ru/authentication/src/main/java/exercise/controller/ArticleController.java
@@ -1,13 +1,14 @@
 package exercise.controller;
 
-import java.util.List;
-
 import exercise.dto.ArticleCreateDTO;
 import exercise.dto.ArticleDTO;
 import exercise.dto.ArticleUpdateDTO;
+import exercise.exception.ResourceNotFoundException;
 import exercise.mapper.ArticleMapper;
+import exercise.repository.ArticleRepository;
 import exercise.repository.UserRepository;
 import exercise.utils.UserUtils;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,9 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import exercise.exception.ResourceNotFoundException;
-import exercise.repository.ArticleRepository;
-import jakarta.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/articles")
@@ -51,7 +50,6 @@ public class ArticleController {
         return articleMapper.map(article);
     }
     // END
-
     @GetMapping("")
     List<ArticleDTO> index() {
         var tasks = articleRepository.findAll();
@@ -60,8 +58,6 @@ public class ArticleController {
                 .map(t -> articleMapper.map(t))
                 .toList();
     }
-
-
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     ArticleDTO show(@PathVariable Long id) {
@@ -69,7 +65,6 @@ public class ArticleController {
                 .orElseThrow(() -> new ResourceNotFoundException("Not Found: " + id));
         return articleMapper.map(article);
     }
-
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     ArticleDTO update(@RequestBody @Valid ArticleUpdateDTO articleData, @PathVariable Long id) {
--- java-spring-ru/authentication/src/main/java/exercise/model/User.java
+++ java-spring-ru/authentication/src/main/java/exercise/model/User.java
@@ -2,28 +2,27 @@ package exercise.model;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
-import jakarta.persistence.Id;
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
-import static jakarta.persistence.GenerationType.IDENTITY;
-
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.ArrayList;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 
 @Entity
